b1ef0ab8790837558812ebbb3dd6b173c83e912e
Zheng Hao Tan <tanzhao@umich.edu>
2015-08-30 02:48:35 -0400

    design changes

--- blink.c
+++ blink.c
@@ -5,11 +5,11 @@
 
 int main ()
 {
-	DDRB |= _BV(DDB0);
+	DDRB |= (1 << PB0);
 
 	while(1) 
 	{
-		PORTB ^= _BV(PB0);
+		PORTB ^= (1 << PB0);
 		_delay_ms(1000);
 		_delay_ms(1000);
 		_delay_ms(1000);
